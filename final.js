527fbaa8e4c6f340fcb3f97545481aa823441a9c
rwang2017 <129701780+rwang2017@users.noreply.github.com>
2023-05-12 16:02:13 -0400

    Update final.js

--- final.js
+++ final.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('visualization-select').addEventListener('change', function(e) {
-      var selectedPart = e.target.value;  // get the selected value
-      var elementToScrollTo = document.getElementById(selectedPart);  // find the corresponding article
-  
-      if (elementToScrollTo) {  // if we found the article
-        elementToScrollTo.scrollIntoView({behavior: "smooth"});  // scroll to it
+      var selectedPart = e.target.value; 
+      var elementToScrollTo = document.getElementById(selectedPart);  
+      if (elementToScrollTo) {  
+        elementToScrollTo.scrollIntoView({behavior: "smooth"}); 
       }
     });
   });
